e22cbe1df3032b8c45a384a1c75f8ea7b20a27a4
larribas <lorenzo.s.arribas@gmail.com>
2021-05-22 14:24:44 +0200

    example: Add an example to demonstrate static parallelization

---
+++ argo_workflows_sdk/examples/static_parallelization.py
@@ -0,0 +1,65 @@
+from typing import Callable, List
+
+import argo_workflows_sdk.inputs as inputs
+import argo_workflows_sdk.outputs as outputs
+from argo_workflows_sdk import DAG, DAGOutput, Node
+
+
+def multiply_by(multiplier: int) -> Callable[[int], int]:
+    def multiply(number: int):
+        return number * multiplier
+
+    return multiply
+
+
+def sum_results(**kwargs) -> int:
+    print(f"The results of all parallel steps, in order, were: {kwargs.values()}")
+    return sum(kwargs.values())
+
+
+number_of_parallel_steps = 10
+
+dag = DAG(
+    nodes={
+        **{
+            f"multiply-by-{i}": Node(
+                multiply_by(i),
+                inputs={
+                    "number": inputs.FromParam(),
+                },
+                outputs={
+                    "multiplied-number": outputs.FromReturnValue(),
+                },
+            )
+            for i in range(number_of_parallel_steps)
+        },
+        "sum-results": Node(
+            sum_results,
+            inputs={
+                f"results-from-{i}": inputs.FromNodeOutput(
+                    node=f"multiply-by-{i}",
+                    output="multiplied-number",
+                )
+                for i in range(number_of_parallel_steps)
+            },
+            outputs={
+                "sum": outputs.FromReturnValue(),
+            },
+        ),
+    },
+    inputs={
+        "number": inputs.FromParam(),
+    },
+    outputs={
+        "sum": DAGOutput(
+            node="sum-results",
+            output="sum",
+        ),
+    },
+)
+
+
+def run_from_cli():
+    from argo_workflows_sdk.runtime.cli import invoke
+
+    invoke(dag)
---
+++ tests/examples/test_static_parallelization.py
@@ -0,0 +1,20 @@
+from typing import Dict
+
+from argo_workflows_sdk.examples.static_parallelization import dag
+from tests.examples.utils import validate_example
+
+
+def validate_results(results: Dict[str, bytes]):
+    assert results == {"sum": b"90"}
+
+
+def test():
+    validate_example(
+        dag,
+        params={
+            "number": b"2",
+        },
+        validate_results=validate_results,
+        argo_workflow_yaml_filename="static_parallelization.yaml",
+        container_entrypoint=["static-parallelization"],
+    )
