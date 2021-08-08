b2b44bffdbf6d363e46b40726791e70e7498f5d0
larribas <lorenzo.s.arribas@gmail.com>
2021-08-08 21:33:35 +0200

    feat: add pickle serializer

--- dagger/serializer/__init__.py
+++ dagger/serializer/__init__.py
@@ -1,6 +1,7 @@
 """Serialization strategies to pass inputs/outputs safely between tasks in a distributed environment."""
 
 from dagger.serializer.as_json import AsJSON  # noqa
+from dagger.serializer.as_pickle import AsPickle  # noqa
 from dagger.serializer.errors import DeserializationError, SerializationError  # noqa
 from dagger.serializer.protocol import Serializer  # noqa
 
