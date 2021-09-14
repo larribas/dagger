8b7bf631f9a58999457b490710cd3b0a5661e4ff
larribas <lorenzo.s.arribas@gmail.com>
2021-09-14 09:32:16 +0200

    feat: perform deep merges of extra runtime options

--- tests/runtime/argo/test_extra_spec_options.py
+++ tests/runtime/argo/test_extra_spec_options.py
@@ -8,19 +8,61 @@ def test__with_extra_options__with_empty_overrides():
     assert with_extra_spec_options(original, {}, "my context") == original
 
 
-def test__with_extra_options__overriding_existing_attributes():
-    original = {"z": 2, "a": 1, "b": 3}
+def test__with_extra_options__with_invalid_values():
+    class InvalidValue:
+        pass
 
     with pytest.raises(ValueError) as e:
-        with_extra_spec_options(original, {"z": 4, "a": 2}, "my context")
+        with_extra_spec_options(
+            {"x": {}},
+            {"x": {"y": InvalidValue()}},
+            "my context",
+        )
 
     assert (
         str(e.value)
-        == "In my context, you are trying to override the value of ['a', 'z']. The Argo runtime uses these attributes to guarantee the behavior of the supplied DAG is correct. Therefore, we cannot let you override them."
+        == "You are trying to set 'my context.x.y' to a value of type 'InvalidValue'. However, only values with a primitive type (None, int, float, str, dict or list) are accepted."
     )
 
 
-def test__with_extra_options__setting_extra_options():
+def test__with_extra_options__with_deep_overrides():
+    original = {
+        "a": {
+            "a": 1,
+            "b": [1],
+        },
+    }
+    assert with_extra_spec_options(
+        original,
+        {
+            "a": {
+                "b": [2],
+                "c": 2,
+            },
+        },
+        "my context",
+    ) == {
+        "a": {
+            "a": 1,
+            "b": [1, 2],
+            "c": 2,
+        }
+    }
+
+
+def test__with_extra_options__overriding_existing_attributes():
+    original = {"z": 2, "a": {"a": 1}, "b": 3}
+
+    with pytest.raises(ValueError) as e:
+        with_extra_spec_options(original, {"y": 3, "a": {"a": 2}}, "my context")
+
+    assert (
+        str(e.value)
+        == "You are trying to override the value of 'my context.a.a'. The Argo runtime already sets a value for this key, and it uses it to guarantee the correctness of the behavior. Therefore, we cannot let you override them."
+    )
+
+
+def test__with_extra_options__setting_extra_options_that_didnt_exist():
     original = {"a": 1}
     assert with_extra_spec_options(original, {"b": 2, "c": 3}, "my context") == {
         "a": 1,
