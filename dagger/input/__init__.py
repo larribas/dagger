eaef92e26cde912b0d9670c79418c44c9300897f
Pablo Barbero <pablo.barbero@glovoapp.com>
2021-11-26 18:58:41 +0100

    format: run isort

--- dagger/input/__init__.py
+++ dagger/input/__init__.py
@@ -3,6 +3,6 @@
 from dagger.input.empty_default_value import EmptyDefaultValue  # noqa
 from dagger.input.from_node_output import FromNodeOutput  # noqa
 from dagger.input.from_param import FromParam  # noqa
+from dagger.input.validators import filter_not_required_inputs  # noqa
 from dagger.input.validators import validate_name  # noqa
 from dagger.input.validators import validate_parameters  # noqa
-from dagger.input.validators import filter_not_required_inputs  # noqa
