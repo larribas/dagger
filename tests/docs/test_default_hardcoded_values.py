aec67e2ed4878700c0b2f961bb400d4e1e0b3469
Pablo Barbero <pablo.barbero@glovoapp.com>
2022-01-19 09:03:01 +0100

    fix: change test name

--- tests/docs/test_default_hardcoded_values.py
+++ tests/docs/test_default_hardcoded_values.py
@@ -3,6 +3,6 @@ from dagger.runtime.local import invoke
 from docs.code_snippets.default_hardcoded_values.imperative import d
 
 
-def test__d__returns_3():
+def test__d__returns_correct_value():
     dag = dsl.build(d)
     assert invoke(dag) == {"return_value": "10-2-20"}
